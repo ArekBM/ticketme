f092f60b9ba9050cd68204812d3e51c0cd90a476
ArekBM <eric.bmiyake@gmail.com>
2023-11-25 13:57:04 -0800

    first commit

---
+++ app/TicketPage/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TicketPage = () => {
+    return (
+        <>
+            <div>
+                TicketPage 
+            </div>
+        </>
+    )
+}
+
+export default TicketPage
