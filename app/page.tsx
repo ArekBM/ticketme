8d8e5ef055d6f56f511b836ca2f3954aada2a701
ArekBM <eric.bmiyake@gmail.com>
2023-11-25 14:30:23 -0800

    finished TicketCard components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import Image from 'next/image'
+import TicketCard from './components/TicketCard'
 
 
-export default function Home() {
+export default function Dashboard() {
   return (
     <>
-      <div>
-        DashBoard
+      <div className='p-5'>
+        <div className='lg:grid grid-cols-2 xl:grid-cols-4'>
+          <TicketCard />
+          <TicketCard />
+          <TicketCard />
+          <TicketCard />
+        </div>
       </div>
     </>
   )
